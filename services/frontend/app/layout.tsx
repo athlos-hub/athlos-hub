d94e17982fb7e55c5d40cc66183cba3cd83a3c4b
Gustavo Emanuel Silva Queiroz <gustavoe.dev@gmail.com>
2025-11-23 22:27:15 -0300

    adiciona header com navegação principal, dropdown interativo e dados de menu; integra react-icons e configura cor principal no Tailwind

--- services/frontend/app/layout.tsx
+++ services/frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
 import { Outfit, Bebas_Neue } from "next/font/google";
+import PlayerHeader from "@/components/layout/player/header";
 
 const outfit = Outfit({
     variable: "--font-outfit",
@@ -23,14 +24,14 @@ export const metadata: Metadata = {
     description: "Plataforma multi-organizacional que transforma a gestão de torneios em uma experiência simples e transparente. Acompanhe jogos ao vivo, estatísticas em tempo real e conecte-se com a comunidade esportiva.",
     icons: {
         icon: [
-            { url: '/favicon.ico' },
-            { url: '/genfavicon-16.png', sizes: '16x16', type: 'image/png' },
-            { url: '/genfavicon-32.png', sizes: '32x32', type: 'image/png' },
-            { url: '/genfavicon-180.png', sizes: '180x180', type: 'image/png' },
-            { url: '/genfavicon-512.png', sizes: '512x512', type: 'image/png' },
+            { url: '/favicons/favicon.ico' },
+            { url: '/favicons/genfavicon-16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicons/genfavicon-32.png', sizes: '32x32', type: 'image/png' },
+            { url: '/favicons/genfavicon-180.png', sizes: '180x180', type: 'image/png' },
+            { url: '/favicons/genfavicon-512.png', sizes: '512x512', type: 'image/png' },
         ],
         apple: [
-            { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+            { url: '/favicons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
         ],
     },
 };
@@ -45,7 +46,10 @@ export default function RootLayout({
       <body
           className={`${outfit.variable} ${bebasNeue.variable} antialiased`}
       >
-        {children}
+        <PlayerHeader />
+        <main className="min-h-screen px-[5rem]">
+          {children}
+        </main>
       </body>
     </html>
   );
--- services/frontend/app/page.tsx
+++ services/frontend/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Olá mundo</h1>
-        <p>olá mundo</p>
+    <div>
+        <span>Home</span>
     </div>
   );
 }
