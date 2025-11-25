91acf9d28a46a7f4e96e39828e57a789f95f24ec
Gustavo Emanuel Silva Queiroz <gustavoe.dev@gmail.com>
2025-11-25 17:43:02 -0300

    adiciona tela de login, formulário de autenticação e opção de login com Google; ajusta navegação do header e estrutura de layouts

---
+++ services/frontend/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import PlayerHeader from "@/components/layout/player/header";
+
+interface MainLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+    return (
+        <div className="max-w-[80rem] mx-auto w-full px-[2.5rem]">
+            <PlayerHeader />
+            {children}
+        </div>
+    );
+}
