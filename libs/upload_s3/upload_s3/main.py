1b03706bbe9a36fb5d94b0dccc30fe2ca154f6a4
Hian Silva <hiansilacts@gmail.com>
2025-12-04 02:03:15 +0000

    adiciona implementação do upload de arquivos para o S3; inclui configuração de variáveis de ambiente e documentação

---
+++ libs/upload_s3/upload_s3/main.py
@@ -0,0 +1,29 @@
+import boto3
+from uuid import uuid4
+from fastapi import UploadFile
+
+def upload_file(file: UploadFile, aws_access_key_id, aws_secret_access_key, aws_region, aws_bucket):
+
+    file_key = f"media/{uuid4()}_{file.filename}"
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=aws_region
+    )
+
+    s3.upload_fileobj(
+        file.file,
+        aws_bucket,
+        file_key,
+        ExtraArgs={"ContentType": file.content_type}
+    )
+
+    url = f"https://{aws_bucket}.s3.{aws_region}.amazonaws.com/{file_key}"
+
+    return {
+        "key": file_key,
+        "url": url
+    }
+
