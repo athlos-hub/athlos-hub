f69ee3ed0a6f2569c76fbd0c443e5a208374a608
Gustavo Emanuel Silva Queiroz <gustavoe.dev@gmail.com>
2026-01-12 16:48:08 -0300

    auto-finaliza lives abandonadas após 10min de inatividade

--- services/livestream-service/src/app.module.ts
+++ services/livestream-service/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module.js';
 import { EnvModule } from './config/env.module.js';
 import { LivesModule } from './lives/lives.module.js';
@@ -7,6 +8,14 @@ import { RedisModule } from './redis/redis.module.js';
 import { WebhooksModule } from './webhooks/webhooks.module.js';
 
 @Module({
-  imports: [EnvModule, PrismaModule, RedisModule, AuthModule, LivesModule, WebhooksModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    EnvModule,
+    PrismaModule,
+    RedisModule,
+    AuthModule,
+    LivesModule,
+    WebhooksModule,
+  ],
 })
 export class AppModule {}
--- services/livestream-service/src/webhooks/webhooks.module.ts
+++ services/livestream-service/src/webhooks/webhooks.module.ts
@@ -3,19 +3,14 @@ import { OnPublishDoneWebhookController } from './controllers/on-publish-done-we
 import { MediaMTXAuthController } from './controllers/mediamtx-auth.controller.js';
 import { ValidateStreamKeyService } from './services/validate-stream-key.service.js';
 import { AutoFinishLiveService } from './services/auto-finish-live.service.js';
+import { CheckAbandonedLivesService } from './services/check-abandoned-lives.service.js';
 import { LivesModule } from '../lives/lives.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
   imports: [LivesModule, PrismaModule],
-  controllers: [
-    OnPublishDoneWebhookController,
-    MediaMTXAuthController,
-  ],
-  providers: [
-    ValidateStreamKeyService,
-    AutoFinishLiveService,
-  ],
+  controllers: [OnPublishDoneWebhookController, MediaMTXAuthController],
+  providers: [ValidateStreamKeyService, AutoFinishLiveService, CheckAbandonedLivesService],
   exports: [ValidateStreamKeyService, AutoFinishLiveService],
 })
 export class WebhooksModule {}
