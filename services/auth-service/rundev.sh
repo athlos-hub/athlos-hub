54a8cd4c63f5ee5ee64b1d033b260a5fb3444966
Gustavo Emanuel Silva Queiroz <gustavoe.dev@gmail.com>
2026-01-19 23:06:32 -0300

    corrige a definição da variável PYTHONPATH nos scripts de execução para evitar duplicação de caminhos

--- services/auth-service/rundev.sh
+++ services/auth-service/rundev.sh
@@ -2,6 +2,6 @@
 
 export PYTHONDONTWRITEBYTECODE=1
 export OAUTHLIB_INSECURE_TRANSPORT=1
-export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
+export PYTHONPATH="$(pwd)/src"
 
 poetry run uvicorn auth_service.main:app --host 0.0.0.0 --port 8000 --reload
