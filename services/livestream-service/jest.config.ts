f840e7fa48258b5a28eb3609190079939e964d75
Hian Silva <hiansilacts@gmail.com>
2026-01-30 20:08:29 -0300

    test: configure Jest and TypeScript for unit tests

---
+++ services/livestream-service/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.spec.json',
+    }],
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>', '<rootDir>/../'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+    '(\\.{1,2}/.*)\\.js$': '$1',
+    '@prisma/client': '<rootDir>/../__mocks__/prisma-client.ts',
+  },
+};
+
+export default config;
